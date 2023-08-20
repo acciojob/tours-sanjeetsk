fa72c55dbd4763ca003ee1b0dbf2bbfe6fa4d3df
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-08-20 16:17:53 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -83,7 +83,7 @@ const App = () => {
                       <p className="tour-info">{tour.info}
                       </p>
                       : 
-                      <p id={`see-more-${tour.id}`}>{`${tour.info.slice(0,200)}...`}
+                      <p id={`see-more-${tour.id}`}>{`${tour.info.slice(0,200)}`}
                       </p>
                     }
                     <button className="show-more-btn" onClick={() => handleToggleInfo(tour.id)}>
